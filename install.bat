5593ce4253740cade8cb5ef65645d247491cbaa4
Elliot Huffman <thelich2@gmail.com>
2013-09-10 11:23:15 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,5 @@
 @echo off
 cls
 echo Microsoft Windows Installer. This will install most versions of Windows that are in WIM format.
+pause
+rem this is just so i can have the file in GitHub. As soon as i put in the main code then i will remove this.
